030b84b7ff35b3964a6a3005f321e1d83a8a3938
okuhlejuly <okuhlejuly2@gmail.com>
2025-10-31 14:53:27 +0200

    Add files via upload

---
+++ perations-types.js
@@ -0,0 +1,7 @@
+let number1 = 28
+let number2 = 36
+const numberstring = string("number1")
+sum = number1 + number2
+console.log("The sum is: " + sum)
+product = number1 * number2
+console.log("The product is: " + product)
